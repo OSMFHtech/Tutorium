685dbf181663a66644dd67bfa26e2811a1a923bf
Name <email>
2023-06-25 16:09:31 +0200

    Initial commit

---
+++ src/main/java/com/example/Tutorium/TutoriumApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tutorium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoriumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoriumApplication.class, args);
+	}
+
+}
